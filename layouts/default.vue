fd8a6f86cd9a14c0c2beb2833656253e879b1703
conoha <xxx>
2021-10-13 19:07:26 +0900

    制作例delete

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,3 +3,17 @@
     <Nuxt />
   </div>
 </template>
+
+<style>
+html {
+  font-family: 'Noto Sans JP', sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  box-sizing: border-box;
+  line-height: 1.5;
+  letter-spacing: 1px;
+  background: #f0ebeb;
+  color: #333;
+  scroll-behavior: smooth;
+}
+</style>
